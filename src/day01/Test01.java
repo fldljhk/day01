25c4b887ce14789940903829615c5f57b758de3c
Your Name <you@example.com>
2022-05-13 20:48:29 +0900

    테스트 작성

--- src/day01/Test01.java
+++ src/day01/Test01.java
@@ -1,5 +1,14 @@
 package day01;
 
 public class Test01 {
-
+	public static void main(String[] args) {
+		//method는 기능이라고 표현한다.
+		//println는 마지막에 엔터가 자동으로 적용된다.
+		/*
+		System.out.println("안녕하세요");
+		System.out.println("반갑습니다");		
+		*/
+		System.out.print("안녕하세요");
+		System.out.print("반갑습니다");	
+	}
 }
---
+++ src/day01/Test02.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class Test02 {
+	public static void main(String[] args) {
+			System.out.println("이름:김지수");						
+			System.out.println("나이:20살");
+			System.out.println("주소:경기도");
+			
+			// \n : 엔터와 비슷, \t : 탭 크기만큼 왼쪽으로 이동
+			// \" : "(큰 따옴표 표현), \\ : (역 슬래시 하나 표현)
+			System.out.println("안\n녕\n하\n세\n요");
+			System.out.println("12345678\t12\t12");
+			System.out.println("\"안녕하세요\"");
+			System.out.println("안\\녕");
+	}
+	
+}
+
+
+
+
